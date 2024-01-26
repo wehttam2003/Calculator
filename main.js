c5b735d27fb672583b8be54b7deae80236a13a0a
Matthew <mdav121503@gmail.com>
2024-01-26 14:28:51 -0600

    add operate function

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function divide(a,b){
     return a / b;
 }
 
+function operate(a,operator,b){
+    switch(operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+
+}
